f8faec174bd7df8de02a5202d65718731d152c89
Ulises Esquivel <uesquivel95@gmail.com>
2022-09-29 19:27:05 -0300

    checkp

---
+++ OB/numbers.js
@@ -0,0 +1,6 @@
+var alturaCM = 165;
+var alturaM = 1.30;
+var PesoKg = 85.3;
+var alturaMrup = Math.ceil(alturaM)
+var alturaMrdown = Math.floor(alturaM);
+var siMax = Number.MAX_VALUE + 1 === Number.MAX_VALUE ;
