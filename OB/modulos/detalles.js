a8261d1f49a47f2070154c5573dce2bc27c791af
Ulises Esquivel <uesquivel95@gmail.com>
2022-10-02 11:44:38 -0300

    checkpoint

---
+++ OB/modulos/detalles.js
@@ -0,0 +1,11 @@
+function factorialf(){
+    var a = 1;
+    for (let i = 1; i < 11; i++){
+    var a = a * i;
+        console.log(a)
+    }
+    
+    }
+    module.exports = {
+        factorialf
+    }
---
+++ OB/modulos/index.js
@@ -0,0 +1,2 @@
+const moduloDetalles = require(".\\modulos\\detalles.js")
+console.log(moduloDetalles.factorialf());
